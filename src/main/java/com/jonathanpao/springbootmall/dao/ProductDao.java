15df55e7f8d6e895bf830c2c90b09086f564a93f
Jonathan <jonathan66652@gmail.com>
2023-02-13 15:08:15 +0800

    商品功能 - 實作查詢商品 (基礎實作)

--- src/main/java/com/jonathanpao/springbootmall/dao/ProductDao.java
+++ src/main/java/com/jonathanpao/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.jonathanpao.springbootmall.dao;
 import com.jonathanpao.springbootmall.dto.ProductRequest;
 import com.jonathanpao.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/jonathanpao/springbootmall/service/ProductService.java
+++ src/main/java/com/jonathanpao/springbootmall/service/ProductService.java
@@ -3,8 +3,11 @@ package com.jonathanpao.springbootmall.service;
 import com.jonathanpao.springbootmall.dto.ProductRequest;
 import com.jonathanpao.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
